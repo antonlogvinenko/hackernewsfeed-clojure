9879febb2bad9b6fd079cc877b34fe19b3c4cc63
Anton Logvinenko <logvinenko@farpost.com>
2013-06-20 10:32:14 +1100

    master Fixed an awful bug in news filtering

--- src/hacker_news_feed/feed.clj
+++ src/hacker_news_feed/feed.clj
@@ -42,7 +42,7 @@
     (merge-with merge ranks postings)))
 
 (defn postings-filter [posting]
-  (-> posting second :rank (> 50)))
+  (-> posting second :rank (> 42)))
 
 (defn get-postings []
   (->> (get-entries)
