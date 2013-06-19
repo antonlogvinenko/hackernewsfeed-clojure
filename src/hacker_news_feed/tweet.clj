2fd3082d2db0edfb52575d3fdc08e60332669b33
Anton Logvinenko <logvinenko@farpost.com>
2013-06-19 23:25:43 +1100

    master Tweeting text

--- src/hacker_news_feed/tweet.clj
+++ src/hacker_news_feed/tweet.clj
@@ -5,9 +5,14 @@
 
 (def my-creds (apply make-oauth-creds (-> "secret" slurp (split #"\n"))))
 
-(defn tweet [message]
+
+(defn update-status [message]
   (statuses-update :oauth-creds my-creds
                    :params {:status message}))
 
+
+(defn tweet [text link]
+  (update-status
+   (str text " " link)))
 
 
