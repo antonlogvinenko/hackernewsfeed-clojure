6b8818ac2e00c53e53346d514d9c12705c2fc20d
Anton Logvinenko <logvinenko@farpost.com>
2013-06-19 22:44:23 +1100

    master Wow, refactoring

--- src/hacker_news_feed/tweet.clj
+++ src/hacker_news_feed/tweet.clj
@@ -1,14 +1,9 @@
 (ns hacker-news-feed.tweet
-  (:use
-   [clojure.string :only (split)]
-   [twitter.oauth]
-   [twitter.callbacks]
-   [twitter.callbacks.handlers]
-   [twitter.api.restful]))
-
+  (:use [clojure.string :only (split)]
+        [twitter.oauth]
+        [twitter.api.restful]))
 
 (def my-creds (apply make-oauth-creds (-> "secret" slurp (split #"\n"))))
-
 
 (defn tweet [message]
   (statuses-update :oauth-creds my-creds
