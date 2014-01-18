45bab03dcd8f9783aaf3395a17f7e7c70a32d6b8
Anton Logvinenko <logvinenko@farpost.com>
2014-01-18 13:37:47 +1100

    master Updated twitter client

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/clojure "1.5.1"]
-                 [twitter-api "0.7.4"]
+                 [twitter-api "0.7.5"]
                  [org/clojars/scsibug/feedparser-clj "0.4.0"]
                  [org.clojure/data.json "0.2.2"]
                  [org.clojure/clojure-contrib "1.2.0"]
