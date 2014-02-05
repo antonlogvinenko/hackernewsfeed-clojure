5d6dd246881937cc94b02fcdc57a9f53e7fd7165
Anton Logvinenko <logvinenko@farpost.com>
2014-02-05 19:00:05 +1100

    master Build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-lein clean uberjar
+lein clean
+lein uberjar
 cd target
 mv hacker-news-feed-0.1.0-SNAPSHOT-standalone.jar hacker-news-feed.jar
 scp hacker-news-feed.jar clojure-host:/home/anton
+ssh clojure-host
